1001211dd5200b4dad3fdf1a6eb07b4624d5a524
DorianQA-WEB <qa.doriangrey@gmail.com>
2026-06-10 23:55:14 +0500

    get user

--- db/dals.py
+++ db/dals.py
@@ -1,6 +1,6 @@
 from uuid import UUID
 from typing import Union
-from sqlalchemy import update, and_
+from sqlalchemy import update, and_, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.models import User
@@ -38,3 +38,10 @@ class UserDAL:
         deleted_user_id_row = result.fetchone()
         if deleted_user_id_row is not None:
             return deleted_user_id_row[0]
+
+    async def get_user_by_id(self, user_id: UUID) -> Union[User, None]:
+        query = select(User).where(User.user_id == user_id)
+        response = await self.db_session.execute(query)
+        user_row = response.fetchone()
+        if user_row is not None:
+            return user_row[0]
